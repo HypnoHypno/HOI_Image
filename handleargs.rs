f46d649ab57f747fdb31f7643d4a95e5c946a4e2
HypnoHypno <153508203+HypnoHypno@users.noreply.github.com>
2024-02-02 15:04:50 -0800

    Update handleargs.rs

--- handleargs.rs
+++ handleargs.rs
@@ -7,14 +7,14 @@ use std::{
 };
 
 pub fn handle_args(vec_args: Vec<String>, vec_args_length: usize) {
-	let show = vec_args_length == 2 && vec_args[2].to_lowercase() == "true"; // Keep in mind usize is one lower than our actual size.
-	let ext = Path::new(&vec_args[1]).extension().and_then(OsStr::to_str);
+	let save = vec_args_length == 2 && vec_args[2].to_lowercase() == "true"; // Keep in mind usize is one lower than our actual size.
+	let ext = Path::new(&vec_args[1]).extension().and_then(OsStr::to_str); // Can't for the life of me figure out what and_then does here. Oh the joy of copy and pasting code. :)
 	match ext {
 		Some("hoi") | Some("hoif") => {
-			convertfromhoi::convert(vec_args[1], show);
+			convertfromhoi::convert(vec_args, save);
 		},
 		_ => {
-			converttohoi::convert(vec_args[1]);
+			converttohoi::convert(vec_args);
 		}
 	}
 }
