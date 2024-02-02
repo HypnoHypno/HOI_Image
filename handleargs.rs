2359a549dd37808923513486e92c4c6b42517008
HypnoHypno <153508203+HypnoHypno@users.noreply.github.com>
2024-02-02 14:43:15 -0800

    Update handleargs.rs

--- handleargs.rs
+++ handleargs.rs
@@ -1,5 +1,20 @@
+mod converttohoi;
+mod convertfromhoi;
+
+use std::{
+	path::Path,
+	ffi::OsStr
+};
+
 pub fn handle_args(vec_args: Vec<String>, vec_args_length: usize) {
-	let show = vec_args_length == 2 && vec_args[2].to_lowercase() == "true"; // Will be true if the second argument passed is true.
-	// Keep in mind usize is one lower than our actual size.
-	print!("{:?}", show)
+	let show = vec_args_length == 2 && vec_args[2].to_lowercase() == "true"; // Keep in mind usize is one lower than our actual size.
+	let ext = Path::new(&vec_args[1]).extension().and_then(OsStr::to_str);
+	match ext {
+		Some("hoi") | Some("hoif") => {
+			convertfromhoi::convert(vec_args[1], show);
+		},
+		_ => {
+			converttohoi::convert(vec_args[1]);
+		}
+	}
 }
