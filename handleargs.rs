860bb81ec523149925bebf31605b003e8a90fa11
HypnoHypno <153508203+HypnoHypno@users.noreply.github.com>
2024-02-02 10:51:32 -0800

    Make better comments

--- handleargs.rs
+++ handleargs.rs
@@ -1,4 +1,5 @@
 pub fn handle_args(vec_args: Vec<String>, vec_args_length: usize) {
-	let show = vec_args_length == 2 && vec_args[2].to_lowercase() == "true"; // Keep in mind usize is one lower than our actual size.
+	let show = vec_args_length == 2 && vec_args[2].to_lowercase() == "true"; // Will be true if the second argument passed is true.
+	// Keep in mind usize is one lower than our actual size.
 	print!("{:?}", show)
 }
